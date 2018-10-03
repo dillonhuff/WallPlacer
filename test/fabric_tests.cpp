1ad5a6bc9a883dbf9c962a2be18f016896f4c79e
Dillon Huff <dillonhuff@gmail.com>
2018-10-02 22:55:03 -0700

    Adding a more realistic example

--- test/fabric_tests.cpp
+++ test/fabric_tests.cpp
@@ -40,7 +40,6 @@ namespace WallPlacer {
         bool foundNode = false;
         for (int r = 0; r < f.numRows(); r++) {
           for (int c = 0; c < f.numCols(); c++) {
-            cout << "Vertex at " << r << ", " << c << " = " << f.vertexAt(r, c) << endl;
             if (f.vertexAt(r, c) == node) {
               foundNode = true;
               break;
@@ -54,6 +53,37 @@ namespace WallPlacer {
       }
 
       REQUIRE(allPlaced);
+    }
+
+    SECTION("4 x 4 fabric with ios on edges") {
+      Fabric f(6, 6);
+
+      OpType compute = 1;
+      OpType load = 2;
+      OpType store = 3;
+
+      for (int r = 1; r < f.numRows() - 1; r++) {
+        f.addOpToTile(r, 0, load);
+        f.addOpToTile(r, 0, store);
+
+        f.addOpToTile(r, 5, load);
+        f.addOpToTile(r, 5, store);
+      }
+
+      for (int c = 1; c < f.numCols() - 1; c++) {
+        f.addOpToTile(0, c, load);
+        f.addOpToTile(0, c, store);
+
+        f.addOpToTile(5, c, load);
+        f.addOpToTile(5, c, store);
+      }
+      
+      for (int r = 1; r < f.numRows() - 1; r++) {
+        for (int c = 1; c < f.numCols() - 1; c++) {
+          f.addOpToTile(r, c, compute);
+        }
+      }
+      
     }
   }
 
